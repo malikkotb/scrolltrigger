74ddd8f4b03cad832b966ce3402f40baaf572dd1
Malik Kotb <malikkotb@icloud.com>
2024-04-11 07:32:38 +0200

    setup

---
+++ app/scrolltrigger/page.js
@@ -0,0 +1,24 @@
+export default function page() {
+  return (
+    <div className="body">
+      <section></section>
+      <section className="middle">
+        <div className="mask h2">
+          <h2>sophen</h2>
+        </div>
+
+        <div className="card card1">
+            <div className="mask"><h3>advanced<br />interactions</h3></div>
+            <div className="mask"><p>Equipped with the latest tech to create the fanciest interactions.</p></div>
+            <div className="box"></div>
+        </div>
+        <div className="card card2">
+            <div className="mask"><h3>extended<br />easing</h3></div>
+            <div className="mask"><p>Utilize our expert flow protocol to extend easing capabilities.</p></div>
+            <div className="box"></div>
+        </div>
+      </section>
+      <section></section>
+    </div>
+  );
+}
